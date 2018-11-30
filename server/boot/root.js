6b01a05d66f74f58ece162f60fbd9b718580de06
carlos ramirez <carlos.ramirez1@unmsm.edu.pe>
2018-11-30 12:30:00 -0500

    hchanges

--- server/boot/root.js
+++ server/boot/root.js
@@ -4,6 +4,16 @@ module.exports = function(server) {
   // Install a `/` route that returns server status
   var router = server.loopback.Router();
   router.get('/', server.loopback.status());
+
+  router.get('/nuevaApp', (req,res) => {
+    return res.status(200).send({meq: "meq"})
+  });
+
+
+
+
+
+
   server.use(router);
-  
+
 };
